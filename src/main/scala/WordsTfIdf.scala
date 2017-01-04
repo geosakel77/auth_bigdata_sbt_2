332408da5477cc9346cdba69cd8db2647381483c
george <mai1428@uom.edu.gr>
2017-01-04 20:16:57 +0200

    wordstfidf

--- src/main/scala/WordsTfIdf.scala
+++ src/main/scala/WordsTfIdf.scala
@@ -1,49 +1,59 @@
 
 
-import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.feature.IDF
 import org.apache.spark.ml.feature.HashingTF
 import org.apache.spark.ml.feature.Tokenizer
-
+import org.apache.spark.ml.feature.NGram
 
 
 object WordsTfIdf {
 
-
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("ML Auth App").setMaster("local[1]")
-    val sc = new SparkContext(conf)
-    val currentDir = System.getProperty("user.dir")
-    println(currentDir)
-    val inputFile = "file://"+currentDir+"/sampleData.csv"
-    println(inputFile)
-    val trainData = sc.textFile(inputFile,2)
-    trainData.foreach(line =>println(line))
-    val seqdata = Seq(trainData.foreach(line=>line.split(',')))
-    /*
-    val sentenceData = spark.createDataFrame(Seq(
-      (0, "Hi I heard about Spark"),
-      (0, "I wish Java could use case classes"),
-      (1, "Logistic regression models are neat")
-    )).toDF("label", "sentence")
+    var csvfilename: String = ""
+    if (args.length != 0) {
 
-    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
-    val wordsData = tokenizer.transform(sentenceData)
-    val hashingTF = new HashingTF()
-      .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(20)
-    val featurizedData = hashingTF.transform(wordsData)
-    // alternatively, CountVectorizer can also be used to get term frequency vectors
+      csvfilename = args(0)
+      if (!(csvfilename == "--help")) {
+        val conf = new SparkConf().setAppName("ML Auth App").setMaster("local[1]")
+        val spark = SparkSession.builder().config(conf).getOrCreate()
+        val currentDir = System.getProperty("user.dir")
+        println(currentDir)
+        val inputFile = "file://" + currentDir + "/"+csvfilename
+        println(inputFile)
+        val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toInt))
+        val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
+        val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
+        val wordsData = tokenizer.transform(dfdata)
+        //----------------------Simple Tf IDF----------------------------------------------
+        val hashingTF1 = new HashingTF().setInputCol("words").setOutputCol("rawFeatures")
+        val feautirizedData = hashingTF1.transform(wordsData)
+        val idf1 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+        val idfModel1 = idf1.fit(feautirizedData)
+        val rescaledData1 = idfModel1.transform(feautirizedData)
+        val puretfidf1 =rescaledData1.select("features","label").rdd
+        //rescaledData1.printSchema()
+        //println(rescaledData.head())
+        //----------------------N gram TF IDF-------------------------------------------------------
+        val ngram = new NGram().setInputCol("words").setOutputCol("ngrams")
+        val ngramWordsData = ngram.transform(wordsData)
+        val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("rawFeatures")
+        val ngramfeautirizedData = hashingTF2.transform(ngramWordsData)
+        val idf2 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+        val idfModel2 = idf2.fit(feautirizedData)
+        val rescaledData2 = idfModel2.transform(feautirizedData)
+        val puretfidf2 =rescaledData1.select("features","label").rdd
+        //puretfidf2.foreach(line => println(line))
 
-    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-    val idfModel = idf.fit(featurizedData)
-    val rescaledData = idfModel.transform(featurizedData)
-    rescaledData.select("features", "label").take(3).foreach(println)
-    */
+      } else {
+        println("scala WordsTfIdf.scala csv_file_name")
+      }
 
+    } else {
+      println("Please provide file path.")
 
-
+    }
 
   }
 
