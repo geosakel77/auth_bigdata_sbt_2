f74275b987c9fb2096855a69866ba59d8d4933cd
george <mai1428@uom.edu.gr>
2017-01-10 07:14:38 +0200

    wordstfidf functions

--- src/main/scala/WordsTfIdf.scala
+++ src/main/scala/WordsTfIdf.scala
@@ -1,7 +1,7 @@
 
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.ml.feature.IDF
 import org.apache.spark.ml.feature.HashingTF
 import org.apache.spark.ml.feature.Tokenizer
@@ -9,6 +9,37 @@ import org.apache.spark.ml.feature.NGram
 
 
 object WordsTfIdf {
+
+
+  def tfidf(spark: SparkSession,inputFile:String): Unit ={
+    val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toInt))
+    val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
+    val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
+    val wordsData = tokenizer.transform(dfdata)
+    //----------------------Simple Tf IDF----------------------------------------------
+    val hashingTF1 = new HashingTF().setInputCol("words").setOutputCol("rawFeatures")
+    val feautirizedData = hashingTF1.transform(wordsData)
+    val idf1 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel1 = idf1.fit(feautirizedData)
+    val rescaledData1 = idfModel1.transform(feautirizedData)
+    val puretfidf1 =rescaledData1.select("features","label").rdd
+  }
+
+  def ngramtfidf(spark: SparkSession,inputFile:String): Unit ={
+    val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toInt))
+    val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
+    val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
+    val wordsData = tokenizer.transform(dfdata)
+    //----------------------N gram TF IDF-------------------------------------------------------
+    val ngram = new NGram().setInputCol("words").setOutputCol("ngrams")
+    val ngramWordsData = ngram.transform(wordsData)
+    val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("rawFeatures")
+    val ngramfeautirizedData = hashingTF2.transform(ngramWordsData)
+    val idf2 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel2 = idf2.fit(ngramfeautirizedData)
+    val rescaledData2 = idfModel2.transform(ngramfeautirizedData)
+    val puretfidf2 =rescaledData2.select("features","label").rdd
+  }
 
   def main(args: Array[String]): Unit = {
     var csvfilename: String = ""
@@ -41,10 +72,12 @@ object WordsTfIdf {
         val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("rawFeatures")
         val ngramfeautirizedData = hashingTF2.transform(ngramWordsData)
         val idf2 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-        val idfModel2 = idf2.fit(feautirizedData)
-        val rescaledData2 = idfModel2.transform(feautirizedData)
-        val puretfidf2 =rescaledData1.select("features","label").rdd
+        val idfModel2 = idf2.fit(ngramfeautirizedData)
+        val rescaledData2 = idfModel2.transform(ngramfeautirizedData)
+        val puretfidf2 =rescaledData2.select("features","label").rdd
         //puretfidf2.foreach(line => println(line))
+
+
 
       } else {
         println("scala WordsTfIdf.scala csv_file_name")
