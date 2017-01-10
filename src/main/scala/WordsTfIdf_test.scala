94912a002c686ec7f0187637f49bc8e3598c2354
george <mai1428@uom.edu.gr>
2017-01-10 15:39:45 +0200

    functional 1

--- src/main/scala/WordsTfIdf_test.scala
+++ src/main/scala/WordsTfIdf_test.scala
@@ -1,16 +1,13 @@
 
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{Dataset, SparkSession,Row}
-import org.apache.spark.ml.feature.IDF
-import org.apache.spark.ml.feature.HashingTF
-import org.apache.spark.ml.feature.Tokenizer
-import org.apache.spark.ml.feature.NGram
 import org.apache.spark.ml.classification.NaiveBayes
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{HashingTF, IDF, NGram, Tokenizer}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 
-object WordsTfIdf {
+object WordsTfIdf_test {
 
 
   def tfidf(spark: SparkSession,inputFile:String,splitRate:Array[Double]): Array[Dataset[Row]] ={
@@ -27,19 +24,16 @@ object WordsTfIdf {
     rescaledData.randomSplit(splitRate)
   }
 
-  def ngramtf(spark: SparkSession,inputFile:String,splitRate:Array[Double],nValue:Int=2): Array[Dataset[Row]] ={
+  def ngramtf(spark: SparkSession,inputFile:String): Unit ={
     val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toDouble))
     val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
     val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
     val wordsData = tokenizer.transform(dfdata)
     //----------------------N gram TF -------------------------------------------------------
-    val ngram = new NGram().setInputCol("words").setOutputCol("ngrams").setN(nValue)
+    val ngram = new NGram().setInputCol("words").setOutputCol("ngrams")
     val ngramWordsData = ngram.transform(wordsData)
-    val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("features")
+    val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("rawFeatures")
     val ngramTF = hashingTF2.transform(ngramWordsData)
-    ngramTF.randomSplit(Array(0.6,0.4))
-
-
   }
 
   def main(args: Array[String]): Unit = {
@@ -54,10 +48,30 @@ object WordsTfIdf {
         println(currentDir)
         val inputFile = "file://" + currentDir + "/"+csvfilename
         println(inputFile)
-        // Data in TFIDF
-        //val Array(trainDataIDFTF,testDataIDFTF) = tfidf(spark,inputFile,Array(0.6,0.4))
-        // Data Ngram TF
-        val Array(trainDataIDFTF,testDataIDFTF) = ngramtf(spark,inputFile,Array(0.7,0.3))
+
+        val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toDouble))
+        val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
+        val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
+        val wordsData = tokenizer.transform(dfdata)
+        //----------------------Simple Tf IDF----------------------------------------------
+        val hashingTF1 = new HashingTF().setInputCol("words").setOutputCol("rawFeatures")
+        val feautirizedData = hashingTF1.transform(wordsData)
+        val idf1 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+        val idfModel1 = idf1.fit(feautirizedData)
+        val rescaledData1 = idfModel1.transform(feautirizedData)
+        val Array(trainDataIDFTF,testDataIDFTF) =rescaledData1.randomSplit(Array(0.6,0.4))
+
+        //println(puretfidf1)
+        //rescaledData1.printSchema()
+        //println(rescaledData.head())
+        //----------------------N gram TF -------------------------------------------------------
+        val ngram = new NGram().setInputCol("words").setOutputCol("ngrams")
+        val ngramWordsData = ngram.transform(wordsData)
+        val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("features")
+        val ngramTF = hashingTF2.transform(ngramWordsData)
+        val Array(trainDataIDFTF1,testDataIDFTF1)= ngramTF.randomSplit(Array(0.6,0.4))
+
+
         //-----------------------First Attempt ML------------------------------------------------------------
 
         // Feature selection
