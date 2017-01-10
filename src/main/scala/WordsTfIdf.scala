ce9e31d90d0a17927f77054ccdee8a395260ac45
george <mai1428@uom.edu.gr>
2017-01-10 14:38:50 +0200

    functional

--- src/main/scala/WordsTfIdf.scala
+++ src/main/scala/WordsTfIdf.scala
@@ -6,13 +6,15 @@ import org.apache.spark.ml.feature.IDF
 import org.apache.spark.ml.feature.HashingTF
 import org.apache.spark.ml.feature.Tokenizer
 import org.apache.spark.ml.feature.NGram
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 
 object WordsTfIdf {
 
 
   def tfidf(spark: SparkSession,inputFile:String): Unit ={
-    val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toInt))
+    val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toDouble))
     val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
     val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
     val wordsData = tokenizer.transform(dfdata)
@@ -25,20 +27,16 @@ object WordsTfIdf {
     val puretfidf1 =rescaledData1.select("features","label").rdd
   }
 
-  def ngramtfidf(spark: SparkSession,inputFile:String): Unit ={
-    val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toInt))
+  def ngramtf(spark: SparkSession,inputFile:String): Unit ={
+    val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toDouble))
     val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
     val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
     val wordsData = tokenizer.transform(dfdata)
-    //----------------------N gram TF IDF-------------------------------------------------------
+    //----------------------N gram TF -------------------------------------------------------
     val ngram = new NGram().setInputCol("words").setOutputCol("ngrams")
     val ngramWordsData = ngram.transform(wordsData)
     val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("rawFeatures")
-    val ngramfeautirizedData = hashingTF2.transform(ngramWordsData)
-    val idf2 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-    val idfModel2 = idf2.fit(ngramfeautirizedData)
-    val rescaledData2 = idfModel2.transform(ngramfeautirizedData)
-    val puretfidf2 =rescaledData2.select("features","label").rdd
+    val ngramTF = hashingTF2.transform(ngramWordsData)
   }
 
   def main(args: Array[String]): Unit = {
@@ -53,7 +51,7 @@ object WordsTfIdf {
         println(currentDir)
         val inputFile = "file://" + currentDir + "/"+csvfilename
         println(inputFile)
-        val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toInt))
+        val trainData = spark.sparkContext.textFile(inputFile, 2).map(line => (line.split(",")(0), line.split(",")(1).toDouble))
         val dfdata = spark.createDataFrame(trainData).toDF("comments", "label")
         val tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words")
         val wordsData = tokenizer.transform(dfdata)
@@ -63,20 +61,42 @@ object WordsTfIdf {
         val idf1 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
         val idfModel1 = idf1.fit(feautirizedData)
         val rescaledData1 = idfModel1.transform(feautirizedData)
-        val puretfidf1 =rescaledData1.select("features","label").rdd
+        val Array(trainDataIDFTF,testDataIDFTF) =rescaledData1.randomSplit(Array(0.6,0.4))
+        //println(puretfidf1)
         //rescaledData1.printSchema()
         //println(rescaledData.head())
-        //----------------------N gram TF IDF-------------------------------------------------------
+        //----------------------N gram TF -------------------------------------------------------
         val ngram = new NGram().setInputCol("words").setOutputCol("ngrams")
         val ngramWordsData = ngram.transform(wordsData)
         val hashingTF2 = new HashingTF().setInputCol("ngrams").setOutputCol("rawFeatures")
-        val ngramfeautirizedData = hashingTF2.transform(ngramWordsData)
-        val idf2 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-        val idfModel2 = idf2.fit(ngramfeautirizedData)
-        val rescaledData2 = idfModel2.transform(ngramfeautirizedData)
-        val puretfidf2 =rescaledData2.select("features","label").rdd
-        //puretfidf2.foreach(line => println(line))
+        val ngramTF = hashingTF2.transform(ngramWordsData)
 
+
+
+        //-----------------------First Attempt ML------------------------------------------------------------
+
+        // Feature selection
+        //TODO
+        // Train a NaiveBayes model.
+        //val model1 = new NaiveBayes()
+        //model1.setFeaturesCol("features")
+        val model = new NaiveBayes().fit(trainDataIDFTF)
+
+        // Select example rows to display.
+        val predictions = model.transform(testDataIDFTF)
+        predictions.show()
+
+        // Select (prediction, true label) and compute test error
+        val evaluator = new MulticlassClassificationEvaluator()
+          .setLabelCol("label")
+          .setPredictionCol("prediction")
+          .setMetricName("accuracy")
+        val accuracy = evaluator.evaluate(predictions)
+        println("Accuracy: " + accuracy)
+
+
+
+        //-----------------------First Attempt ML------------------------------------------------------------
 
 
       } else {
