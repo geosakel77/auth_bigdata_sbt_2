d9ec41535f23278bd092891ab7b0c8f0ac5e7593
george <mai1428@uom.edu.gr>
2017-01-21 00:41:37 +0200

    sampling filter

--- src/main/scala/WordsTFIDFRDD.scala
+++ src/main/scala/WordsTFIDFRDD.scala
@@ -6,17 +6,33 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.feature.IDF
+import org.apache.spark.mllib.feature.Normalizer
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.util.MLUtils
 
 object WordsTFIDFRDD {
 
-  def tfidfrdd(sc: SparkContext,inputFile:String,splitRate:Array[Double]): Unit = {
+  def tfidfrdd(sc: SparkContext,inputFile:String,splitRate:Array[Double],norm:Boolean=true): Array[RDD[LabeledPoint]] = {
     val hashingTF = new HashingTF()
 
-    val trainData = sc.textFile(inputFile).map(line =>LabeledPoint.apply(line.split(",")(1).toDouble,hashingTF.transform(line.split(",")(0).replace("\"","").split(" "))))
+    val trainData = sc.textFile(inputFile).map(line =>LabeledPoint.apply(line.split(",")(1).toDouble,hashingTF.transform(line.split(",")(0).replace("\"","").split(" ")))).cache()
+    val hashedTrainingData = trainData.map(_.features)
+    val idfModel = new IDF().fit(hashedTrainingData)
+    val idf = idfModel.transform(hashedTrainingData)
+    val labeledVectors = if (norm==true){
+      val normalizer = new Normalizer()
+      idf.zip(trainData).map(x=>LabeledPoint(x._2.label,normalizer.transform(x._1))).randomSplit(splitRate)
+    }else{
+      idf.zip(trainData).map(x=>LabeledPoint(x._2.label,x._1)).randomSplit(splitRate)
+    }
+    labeledVectors
+  }
 
-    trainData.foreach(row =>print(row))
+  def words2vecrdd():Unit={
+
+
+
+
   }
 
 
@@ -32,8 +48,8 @@ object WordsTFIDFRDD {
         println(currentDir)
         val inputFile = "file://" + currentDir + "/" + csvfilename
         println(inputFile)
-        this.tfidfrdd(sc,inputFile,Array(0.6,0.4))
-
+        val Array(trainData,testData)=this.tfidfrdd(sc,inputFile,Array(0.6,0.4))
+        trainData.foreach(f=>println(f.label+":"+f.features))
 
       } else {
         println("scala WordsTfIdf.scala csv_file_name")
